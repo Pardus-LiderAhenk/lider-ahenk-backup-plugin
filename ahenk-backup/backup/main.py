79d9a760a717abcb327f1cb5de951b09b2d8e396
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-06 16:11:05 +0300

    main module info added

--- ahenk-backup/backup/main.py
+++ ahenk-backup/backup/main.py
@@ -3,6 +3,12 @@
 # Author: 
 
 
-
 def info():
-    return None
+    inf = dict()
+    inf['name'] = 'backup'
+    inf['version'] = '1.0.0'
+    inf['support'] = 'debian'
+    inf['description'] = ''
+    inf['developer'] = ''
+
+    return inf
