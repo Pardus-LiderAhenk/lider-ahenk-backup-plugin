d53b893805635877d365ae665dd810af6a8412af
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-04-16 22:14:19 +0300

    profile and plucin.xml change for lider management editor page

--- lider-console-backup/src/tr/org/liderahenk/backup/handlers/BackupProfileHandler.java
+++ lider-console-backup/src/tr/org/liderahenk/backup/handlers/BackupProfileHandler.java
@@ -1,47 +1,74 @@
 package tr.org.liderahenk.backup.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import tr.org.liderahenk.backup.constants.BackupConstants;
 import tr.org.liderahenk.backup.dialogs.BackupProfileDialog;
 import tr.org.liderahenk.backup.i18n.Messages;
-import tr.org.liderahenk.liderconsole.core.constants.LiderConstants;
 import tr.org.liderahenk.liderconsole.core.editorinput.ProfileEditorInput;
+import tr.org.liderahenk.liderconsole.core.handlers.LiderAbstractHandler;
 
 /**
  * Profile definition handler for backup plugin.
  *
  */
-public class BackupProfileHandler extends AbstractHandler {
+public class BackupProfileHandler extends LiderAbstractHandler {
 
 	private Logger logger = LoggerFactory.getLogger(BackupProfileHandler.class);
-
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-        IWorkbenchPage page = window.getActivePage();
-        
-        try {
-			// Here we open default profile editor implementation so that all
-			// profiles can be handled by Lider Console Core.
-			// We also pass our profile dialog implementation as parameter to
-			// allow the editor use it dynamically.
-			page.openEditor(new ProfileEditorInput(Messages.getString("Backup"), BackupConstants.PLUGIN_NAME, 
-					BackupConstants.PLUGIN_VERSION, new BackupProfileDialog()), 
-					LiderConstants.EDITORS.PROFILE_EDITOR);
-		} catch (PartInitException e) {
-			logger.error(e.getMessage(), e);
-		}
-
-        return null;
+//
+//	public Object execute(ExecutionEvent event) throws ExecutionException {
+//		
+//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+//        IWorkbenchPage page = window.getActivePage();
+//        
+//        String selectedProfileId=event.getParameter("selectedProfileId");
+//        String action=event.getParameter("action");
+//        
+//        
+//        try {
+//			// Here we open default profile editor implementation so that all
+//			// profiles can be handled by Lider Console Core.
+//			// We also pass our profile dialog implementation as parameter to
+//			// allow the editor use it dynamically.,
+//        	
+//        	
+//        	if(action.equals("add")){
+//        		ProfileEditorInput editorInput = new ProfileEditorInput(Messages.getString("Backup"), BackupConstants.PLUGIN_NAME, 
+//						BackupConstants.PLUGIN_VERSION, new BackupProfileDialog()) ;
+//				DefaultProfileDialog dialog = new DefaultProfileDialog(Display.getDefault().getActiveShell(), null,
+//						editorInput);
+//				dialog.create();
+//				dialog.open();
+//        	}
+//        	else if( action.equals("update") && selectedProfileId!=null){
+//        		
+//    				ProfileEditorInput editorInput = new ProfileEditorInput(Messages.getString("Backup"), BackupConstants.PLUGIN_NAME, 
+//    						BackupConstants.PLUGIN_VERSION, new BackupProfileDialog()) ;
+//    				DefaultProfileDialog dialog = new DefaultProfileDialog(Display.getDefault().getActiveShell(), null,	editorInput,selectedProfileId);
+//    				dialog.create();
+//    				dialog.open();
+//        		
+//        	}
+//        	
+//        	
+//        
+////        	else
+////			page.openEditor(new ProfileEditorInput(Messages.getString("Backup"), BackupConstants.PLUGIN_NAME, 
+////					BackupConstants.PLUGIN_VERSION, new BackupProfileDialog()), 
+////					LiderConstants.EDITORS.PROFILE_EDITOR);
+//		} catch (Exception e) {
+//			logger.error(e.getMessage(), e);
+//		}
+//
+//        return null;
+//	}
+	
+	@Override
+	public ProfileEditorInput getEditorInput() {
+		// TODO Auto-generated method stub
+		return new ProfileEditorInput(Messages.getString("Backup"), BackupConstants.PLUGIN_NAME, 
+				BackupConstants.PLUGIN_VERSION, new BackupProfileDialog()) ;
 	}
 
 }
