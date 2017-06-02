5da8d6f49f90db0334a6a35e67ab64e83d84bfb8
İsmail Başaran <basaran.ismaill@gmail.com>
2017-06-02 11:35:07 +0300

    rsync file operations

--- ahenk-backup/backup/api/rsync.py
+++ ahenk-backup/backup/api/rsync.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import subprocess,sys
+import subprocess,sys, os
 from requests.api import options
+from pip.cmdoptions import process_dependency_links
+from pexpect import expect
 
 class BackupRsync():
     def __init__(self, backup_data):
@@ -13,7 +15,7 @@ class BackupRsync():
         destinationPath = self.backup_data['username'] + "@" + self.backup_data['destHost'] + ':' + self.backup_data['destPath']
         for backup_directory in self.backup_data['directories']:
             path = backup_directory['sourcePath'] + ' ' + destinationPath
-            options = ' --info=progress '
+            options = ' -v --progress --stats '
             if source['recursive']:
                 options = options + ' -r '
             if source['preserveGroup']:
@@ -32,3 +34,27 @@ class BackupRsync():
                 options = options + ' --exclude "' + source['excludePattern'] + '" '
         backup_command = 'rsync ' + options + ' ' + path
         return backup_command
+    
+    def execute_command(self,cmd):
+        try:
+            command_process = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            while True:
+                output = process.stdout.readline()
+                if output == '' or process.poll() is not None:
+                    return 0
+                if output:
+                    print('in progress')
+        except Exception as e:
+            return str(e)
+    
+    def getResourceTotalSize(self,sourcePath):
+        return os.stat(sourcePath).st_size
+    
+    def isFile(self,sourcePath):
+        return os.path.isfile(sourcePath)
+    
+    
+    
+    
+    
+    
