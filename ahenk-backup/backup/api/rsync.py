d574e271bb3bed2243d5a056695d359844380f24
İsmail Başaran <basaran.ismaill@gmail.com>
2017-06-09 09:52:04 +0300

    backup parser

--- ahenk-backup/backup/api/rsync.py
+++ ahenk-backup/backup/api/rsync.py
@@ -1,55 +1,63 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import subprocess,sys, os, thread, time
+import subprocess,sys, os, threading, time
 from requests.api import options
 from pip.cmdoptions import process_dependency_links
 from pexpect import expect
 import cmd
+from base.scope import Scope
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
+from sys import stderr
+from rsync_parser import BackupParser
 
 class BackupRsync():
     def __init__(self, backup_data):
         super(BackupRsync, self).__init__()
         self.backup_data = backup_data
-        self.backup_result = []
-        self.is_resume_backup_process = True
+        self.backup_result = {}
+        self.parser = BackupParser()
     
     def prepare_command(self):
         destinationPath = self.backup_data['username'] + "@" + self.backup_data['destHost'] + ':' + self.backup_data['destPath']
         for backup_directory in self.backup_data['directories']:
             path = backup_directory['sourcePath'] + ' ' + destinationPath
-            options = ' -v --progress --stats '
-            if source['recursive']:
+            options = ' -a --no-i-r --info=progress --stats '
+            if backup_directory['recursive']:
                 options = options + ' -r '
-            if source['preserveGroup']:
+            if backup_directory['preserveGroup']:
                 options = options + ' -g '
-            if source['preserveOwner']:
+            if backup_directory['preserveOwner']:
                 options = options + ' -o '
-            if source['preservePermissions']:
+            if backup_directory['preservePermissions']:
                 options = options + ' -p '
-            if source['archive']:
-                options = options + ' -a '
-            if source['compress']:
+            #if backup_directory['archive']:
+            #    options = options + ' -a '
+            if backup_directory['compress']:
                 options = options + ' -z '
-            if source['existingOnly']:
+            if backup_directory['existingOnly']:
                 options = options + ' --existing '
-            if source['excludePattern']:
-                options = options + ' --exclude "' + source['excludePattern'] + '" '
+            if backup_directory['excludePattern']:
+                options = options + ' --exclude "' + backup_directory['excludePattern'] + '" '
         backup_command = 'rsync ' + options + ' ' + path
         return backup_command
     
     def execute_command(self,cmd):
         try:
-            command_process = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            while True:
-                output = process.stdout.readline()
-                if output == '' or process.poll() is not None:
-                    self.backup_result['status'] = 'complated'
+            command_process = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            self.parser.set_process = command_process
+            Scope.get_instance().get_logger().info('Backup baslatiliyor.')
+            while self.parser.resuming:
+                output = command_process.stdout.readline()
+                if output == '' or command_process.poll() is not None:
+                    self.parser.resuming = False
                 if output:
-                    output_arr = output.split()
-                    if len(output_arr) > 1:
-                        self.backup_result['complated_percentage'] = output_arr[2].replace('%', '')
+                    self.parser.parseLine(output)
+            Scope.get_instance().get_logger().info('Backup bitti.')
         except Exception as e:
+            Scope.get_instance().get_logger().info('hataya girdim')
+            Scope.get_instance().get_logger().info(e)
             return str(e)
     
     def get_resource_total_size(self,sourcePath):
@@ -59,28 +67,49 @@ class BackupRsync():
         return os.path.isfile(source_path)
     
     def append_command_execution_type(self,cmd):
-        return 'sshpass -p ' + self.backup_data['password'] + cmd
-    
+        return 'sshpass -p ' + self.backup_data['password'] + ' ' + cmd + ' | stdbuf -oL tr "\\r" "\\n"'
+        
     def backup(self):
-        cmd = self.append_command_execution_type(self.prepare_command())
         self.start_backup_watcher()
+        self.start_backup()
+        
+    def prepare_backup(self):
+        Scope.get_instance().get_logger().info('starting to backup')
+        cmd = self.append_command_execution_type(self.prepare_command())
+        Scope.get_instance().get_logger().info('command ok')
+        
+        Scope.get_instance().get_logger().info('watcher ok')
+        Scope.get_instance().get_logger().info(cmd)
         self.execute_command(cmd)
-    
+        Scope.get_instance().get_logger().info('evet backup bittii')
+        
+        Scope.get_instance().get_logger().info(self.parser.percentage)
+        Scope.get_instance().get_logger().info(self.parser.number_of_transfered_files)
+        
+    def start_backup(self):
+        try:
+            Scope.get_instance().get_logger().info('starting to watcher 4')
+            t = threading.Thread(target=self.prepare_backup, args=())
+            t.start()
+            Scope.get_instance().get_logger().info('starting to watcher 5')
+        except Exception as e:
+            Scope.get_instance().get_logger().info(e)
+            
     def backup_watcher(self):
-        while self.is_resume_backup_process:
+        while self.parser.resuming:
             try:
-                time.sleep(5)
-                if self.backup_result['status'] == 'complated':
-                    print('Finished backup process') # Send this to lider server
-                    self.is_resume_backup_process = False
-                else:
-                    print(self.backup_result['complated_percentage']) # Send this to lider server
+                time.sleep(2)
+                Scope.get_instance().get_logger().info(self.parser.percentage)
+                Scope.get_instance().get_logger().info('Kalan zaman ' + self.parser.estimated_time) # Send this to lider server
             except Exception as e:
                 print(e)
     
     def start_backup_watcher(self):
         try:
-            thread.start_new_thread (backup_watcher, None)
+            Scope.get_instance().get_logger().info('starting to watcher')
+            t = threading.Thread(target=self.backup_watcher, args=())
+            t.start()
+            Scope.get_instance().get_logger().info('starting to watcher 2')
         except Exception as e:
             print(e)
     
--- ahenk-backup/backup/api/rsync_parser.py
+++ ahenk-backup/backup/api/rsync_parser.py
@@ -8,26 +8,30 @@ import subprocess,sys
 
 
 class BackupParser():
-    def __init__(self, sourcePath):
+    def __init__(self):
         super(BackupParser, self).__init__()
-        self.sourcePath = sourcePath
         self.percentage = None
         self.number_of_files = None
         self.number_of_transfered_files = None
         self.resuming = True
+        self.estimated_time = 0
+        
     
     def parseLine(self,line):
-        if output == '' or process.poll() is not None:
-            self.resuming = False
-            break
         if(b'Number of files' in line):
             total_file = re.findall(b'Number of files: (\d+)', line)
             if total_file:
                 self.number_of_files = str(total_file[0].decode('utf-8'))
+                self.percentage = str(100)
+                self.estimated_time = '0:00:00'
+                self.resuming = False
         elif(b'Number' in line and b'transferred' in line):
             transfferd_file_size = re.findall(b'Number of regular files transferred: (\d+)', line)
             if transfferd_file_size:
                 self.number_of_transfered_files = str(transfferd_file_size[0].decode('utf-8'))
+                self.percentage = str(100)
+                self.estimated_time = '0:00:00'
+                self.resuming = False
         elif(b'' == line):
             pass
         else:
@@ -39,3 +43,4 @@ class BackupParser():
             if len(lineAsArr) > 1 and b'%' in line:
                 #print(str(lineAsArr[1].decode('utf-8')).replace('%',''))
                 self.percentage = str(lineAsArr[1].decode('utf-8')).replace('%','')
+                self.estimated_time = str(lineAsArr[3].decode('utf-8'))
