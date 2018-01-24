178dd805b0b2658333473df9ce0fe4b0db238f70
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-01-24 16:42:12 +0300

    version name fix: 1.1

--- ahenk-backup/backup/main.py
+++ ahenk-backup/backup/main.py
@@ -6,7 +6,7 @@
 def info():
     inf = dict()
     inf['name'] = 'backup'
-    inf['version'] = '1.0.0'
+    inf['version'] = '1.1'
     inf['support'] = 'debian'
     inf['description'] = ''
     inf['task'] = True
